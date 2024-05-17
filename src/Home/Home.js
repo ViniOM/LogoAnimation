39ecb596c89228e92b91c438403b297805a4d927
ViniOM <vini.webdeveloper@gmail.com>
2024-05-17 11:18:57 -0300

    feat: inventei legal agora

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,46 @@
 import "./Home.css";
+import { React, useState } from "react";
 
 export default function Home(props) {
+  let [count, setCount] = useState(9);
+
+  const handleClick = () => {
+    setCount(count - 1);
+
+    if (count == 1) {
+      setCount((count = 10));
+    }
+  };
+
   return (
     <div className="home">
-      <h1>Seja bem Vindo a</h1>
-      <h2>Home!</h2>
-      <img src={props.img} alt="" width="100px" className="logo" />
+      <div className="nav">
+        <h1>Seja bem Vindo a Home!</h1>
+      </div>
+
+      <div className="content">
+        <img
+          src={props.img}
+          alt=""
+          width="300px"
+          className="logo"
+          style={{
+            animation: `App-logo-spin infinite ${count}s linear`,
+          }}
+        />
+        <br/>
+        <button className="counter" onClick={handleClick}>
+          Aumentar velocidade
+        </button>
+        <p
+          className="reset"
+          style={{
+            display: count == 10 ? "block" : "none",
+          }}
+        >
+          Reiniciou!
+        </p>
+      </div>
     </div>
   );
 }
