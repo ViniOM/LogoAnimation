855079c54557bf355f9b7418e6aa044088af155b
ViniOM <vini.webdeveloper@gmail.com>
2024-05-17 11:43:58 -0300

    Polindo

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,13 +3,20 @@ import { React, useState } from "react";
 
 export default function Home(props) {
   let [count, setCount] = useState(9);
+  let [change, setChange] = useState(false);
 
   const handleClick = () => {
-    setCount(count - 1);
+    setCount(count - 2);
+    console.log(count);
 
-    if (count == 1) {
-      setCount((count = 10));
+    if (count === 1) {
+      setCount((count = 9));
+      setChange((change = true));
+      console.log(change);
+      return;
     }
+
+    setChange((change = false));
   };
 
   return (
@@ -28,14 +35,16 @@ export default function Home(props) {
             animation: `App-logo-spin infinite ${count}s linear`,
           }}
         />
-        <br/>
+        <br />
         <button className="counter" onClick={handleClick}>
           Aumentar velocidade
         </button>
         <p
           className="reset"
           style={{
-            display: count == 10 ? "block" : "none",
+            display: change === true ? "block" : "none",
+            color: "red",
+            letterSpacing: '1px'
           }}
         >
           Reiniciou!
